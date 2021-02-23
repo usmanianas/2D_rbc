a0a897fcca65e5c62e7a78c365074ea8570635f9
usmanianas <anasusmani2010@gmail.com>
2021-02-23 17:35:20 +0530

    working code

--- CollRrbc.py
+++ CollRrbc.py
@@ -10,7 +10,7 @@ import random
 
 Lx, Lz = 1.0, 1.0
 
-Nx, Nz = 25, 25
+Nx, Nz = 32, 32
 
 hx, hz = Lx/(Nx-1), Lz/(Nz-1)
 
@@ -19,15 +19,11 @@ z = np.linspace(0, 1, Nz, endpoint=True)
 
 hx2, hz2 = hx*hx, hz*hz
 
-hx2hy2, hz2hx2 = hx2*hy2, hz2*hx2
-
-hx2hz2 = hx2*hz2
-
 idx2, idz2 = 1.0/hx2, 1.0/hz2
 
 Ra = 1.0e4
 
-Pr = 1
+Pr = 1.0
 
 print("#", "Ra=", Ra, "Pr=", Pr)
 
@@ -39,7 +35,7 @@ nu, kappa = np.sqrt(Pr/Ra), 1.0/np.sqrt(Ra*Pr)
 
 dt = 0.02
 
-tMax = 1000
+tMax = 100
 
 restart = 0    # 0-Fresh, 1-Restart
 
@@ -55,7 +51,7 @@ VpTolerance = 1.0e-5
 # Tolerance value in Jacobi iterations
 PoissonTolerance = 1.0e-3
 
-gssor = 1.6
+gssor = 1.0
 
 maxCount = 1e4
 
@@ -64,7 +60,9 @@ maxCount = 1e4
 
 def getDiv(U, W):
 
-    divMat = np.zeros([L, M])
+    global Nx, Nz, hx, hz
+
+    divMat = np.zeros([Nx, Nz])
 
     divMat[1:Nx-1, 1:Nz-1] = ((U[2:Nx, 1:Nz-1] - U[0:Nx-2, 1:Nz-1])*0.5/hx +
                             (W[1:Nx-1, 2:Nz] - W[1:Nx-1, 0:Nz-2])*0.5/hz)
@@ -101,7 +99,9 @@ def initFields():
     #T = random.uniform(0, 1) * np.ones([L, M, N])
     T = np.zeros([Nx ,Nz])
 
-    T[:, 0:Nz] = 1 - z[0:Nz]
+    T[:, 0:Nz] = 1.0 - z[0:Nz]
+
+    #print(T)
 
     U = np.zeros([Nx ,Nz])
 
@@ -158,7 +158,6 @@ def TimeIntegrate():
     iCnt = 1
     
     Hx.fill(0.0)
-    Hy.fill(0.0)
     Hz.fill(0.0)
     Ht.fill(0.0)
 
@@ -175,7 +174,6 @@ def TimeIntegrate():
 
     
         Hx = computeNLinDiff_X(U, W)
-        Hy = computeNLinDiff_Y(U, W)
         Hz = computeNLinDiff_Z(U, W)
         Ht = computeNLinDiff_T(U, W, T)  
     
@@ -187,7 +185,7 @@ def TimeIntegrate():
         uJacobi(Hx)
     
         # Calculating guessed values of W implicitly
-        Hz[1:Nx-1, 1:Nz-1] = W[1:Nx-1, 1:Nz-1] + dt*(Hz[1:Nx-1, 1:Nz-1] - ((P[1:Nx-1, 2:Nz] - P[1:Nx-1 0:Nz-2])/(2.0*hz)) + T[1:Nx-1, 1:Nz-1])
+        Hz[1:Nx-1, 1:Nz-1] = W[1:Nx-1, 1:Nz-1] + dt*(Hz[1:Nx-1, 1:Nz-1] - ((P[1:Nx-1, 2:Nz] - P[1:Nx-1, 0:Nz-2])/(2.0*hz)) + T[1:Nx-1, 1:Nz-1])
         wJacobi(Hz)
     
         # Calculating guessed values of T implicitly
@@ -215,15 +213,17 @@ def TimeIntegrate():
         imposeUBCs(U)                               
         imposeWBCs(W)                               
         imposePBCs(P)                               
-        imposeTBCs(T)       
+        imposeTBCs(T) 
+
+        #print(np.amax(U), np.amax(W))      
 
         #if abs(fwTime - time) < 0.5*dt:
         if abs(time - tMax)<1e-5:
             writeSoln(U, W, P, T, time)
             Z, X = np.meshgrid(x,z)
-            plt.contourf(X, Z, T[int(Nx/2), :], 500, cmap=cm.coolwarm)
+            plt.contourf(X, Z, T, 500, cmap=cm.coolwarm)
             clb = plt.colorbar()
-            plt.quiver(X[0:Nx,0:Nz], Z[0:Nx,0:Nz], U[int(Nx/2), :], W[int(Nx/2), :])
+            plt.quiver(X, Z, U, W)
             plt.axis('scaled')
             clb.ax.set_title(r'$T$', fontsize = 20)
             plt.show()
@@ -250,8 +250,8 @@ def computeNLinDiff_X(U, W):
 
     Hx[1:Nx-1, 1:Nz-1] = (((U[2:Nx, 1:Nz-1] - 2.0*U[1:Nx-1, 1:Nz-1] + U[0:Nx-2, 1:Nz-1])/hx2 + 
                                 (U[1:Nx-1, 2:Nz] - 2.0*U[1:Nx-1, 1:Nz-1] + U[1:Nx-1, 0:Nz-2])/hz2)*0.5*nu -
-                              U[1:L-1, 1:N-1]*(U[2:Nx, 1:Nz-1] - U[0:Nx-2, 1:Nz-1])/(2.0*hx) -
-                              W[1:L-1, 1:N-1]*(U[1:Nx-1, 2:Nz] - U[1:Nx-1, 0:Nz-2])/(2.0*hz))
+                              U[1:Nx-1, 1:Nz-1]*(U[2:Nx, 1:Nz-1] - U[0:Nx-2, 1:Nz-1])/(2.0*hx) -
+                              W[1:Nx-1, 1:Nz-1]*(U[1:Nx-1, 2:Nz] - U[1:Nx-1, 0:Nz-2])/(2.0*hz))
 
     return Hx
 
@@ -288,24 +288,26 @@ def uJacobi(rho):
     global U
     global Nx, Nz
 
-    Up = np.zeros_like(rho)
+    #Up = np.zeros_like(rho)
 
     jCnt = 0
     while True:
 
 
-        Up[1:Nx-1, 1:Nz-1] =(1.0/(nu*dt*(idx2 + idz2))) * (rho[1:Nx-1, 1:Nz-1] + 
-                                       0.5*nu*dt*idx2*(Up[0:Nx-2, 1:Nz-1] + Up[2:Nx, 1:Nz-1]) +
-                                       0.5*nu*dt*idz2*(Up[1:Nx-1, 0:Nz-2] + Up[1:Nz-1, 2:Nz]))   
+        U[1:Nx-1, 1:Nz-1] =(1.0/(1+nu*dt*(idx2 + idz2))) * (rho[1:Nx-1, 1:Nz-1] + 
+                                       0.5*nu*dt*idx2*(U[0:Nx-2, 1:Nz-1] + U[2:Nx, 1:Nz-1]) +
+                                       0.5*nu*dt*idz2*(U[1:Nx-1, 0:Nz-2] + U[1:Nz-1, 2:Nz]))   
 
 
-        imposeUBCs(Up)
+        imposeUBCs(U)
         
-        maxErr = np.amax(np.fabs(rho[1:Nx-1, 1:Nz-1] - (Up[1:Nx-1, 1:Nz-1] - 0.5*nu*dt*(
-                            (Up[0:Nx-2, 1:Nz-1] - 2.0*Up[1:Nx-1, 1:Nz-1] + Up[2:Nx, 1:Nz-1])/hx2 +
-                            (Up[1:Nx-1, 0:Nz-2] - 2.0*Up[1:Nx-1, 1:Nz-1] + Up[1:Nx-1, 2:Nz])/hz2))))
+        maxErr = np.amax(np.fabs(rho[1:Nx-1, 1:Nz-1] - (U[1:Nx-1, 1:Nz-1] - 0.5*nu*dt*(
+                            (U[0:Nx-2, 1:Nz-1] - 2.0*U[1:Nx-1, 1:Nz-1] + U[2:Nx, 1:Nz-1])/hx2 +
+                            (U[1:Nx-1, 0:Nz-2] - 2.0*U[1:Nx-1, 1:Nz-1] + U[1:Nx-1, 2:Nz])/hz2))))
         
             #if maxErr < tolerance:
+        #print(maxErr)
+
         if maxErr < VpTolerance:
             #print(jCnt)
             break
@@ -325,23 +327,28 @@ def wJacobi(rho):
     global W
     global Nx, Nz
     
-    Wp = np.zeros_like(rho)
+    #Wp = np.zeros_like(rho)
+
+    #print(np.amax(rho))
     
     jCnt = 0
     while True:
 
-        Wp[1:Nx-1, 1:Nz-1] =(1.0/(nu*dt*(idx2 + idz2))) * (rho[1:Nx-1, 1:Nz-1] + 
-                                       0.5*nu*dt*idx2*(Wp[0:Nx-2, 1:Nz-1] + Wp[2:Nx, 1:Nz-1]) +
-                                       0.5*nu*dt*idz2*(Wp[1:Nx-1, 0:Nz-2] + Wp[1:Nz-1, 2:Nz]))   
+        W[1:Nx-1, 1:Nz-1] =(1.0/(1+nu*dt*(idx2 + idz2))) * (rho[1:Nx-1, 1:Nz-1] + 
+                                       0.5*nu*dt*idx2*(W[0:Nx-2, 1:Nz-1] + W[2:Nx, 1:Nz-1]) +
+                                       0.5*nu*dt*idz2*(W[1:Nx-1, 0:Nz-2] + W[1:Nz-1, 2:Nz]))   
     
-        imposeWBCs(Wp)
+        imposeWBCs(W)
 
 
-        maxErr = np.amax(np.fabs(rho[1:Nx-1, 1:Nz-1] - (Wp[1:Nx-1, 1:Nz-1] - 0.5*nu*dt*(
-                        (Wp[0:Nx-2, 1:Nz-1] - 2.0*Wp[1:Nx-1, 1:Nz-1] + Wp[2:Nx, 1:Nz-1])/hx2 +
-                        (Wp[1:Nx-1, 0:Nz-2] - 2.0*Wp[1:Nx-1, 1:Nz-1] + Wp[1:Nx-1, 2:Nz])/hz2))))
+        maxErr = np.amax(np.fabs(rho[1:Nx-1, 1:Nz-1] - (W[1:Nx-1, 1:Nz-1] - 0.5*nu*dt*(
+                        (W[0:Nx-2, 1:Nz-1] - 2.0*W[1:Nx-1, 1:Nz-1] + W[2:Nx, 1:Nz-1])/hx2 +
+                        (W[1:Nx-1, 0:Nz-2] - 2.0*W[1:Nx-1, 1:Nz-1] + W[1:Nx-1, 2:Nz])/hz2))))
     
         #if maxErr < tolerance:
+
+        #print(maxErr)
+
         if maxErr < 1e-5:
             #print(jCnt)
             break
@@ -366,7 +373,7 @@ def TJacobi(rho):
     jCnt = 0
     while True:
 
-        T[1:Nx-1, 1:Nz-1] =(1.0/(kappa*dt*(idx2 + idz2))) * (rho[1:Nx-1, 1:Nz-1] + 
+        T[1:Nx-1, 1:Nz-1] =(1.0/(1+kappa*dt*(idx2 + idz2))) * (rho[1:Nx-1, 1:Nz-1] + 
                                        0.5*kappa*dt*idx2*(T[0:Nx-2, 1:Nz-1] + T[2:Nx, 1:Nz-1]) +
                                        0.5*kappa*dt*idz2*(T[1:Nx-1, 0:Nz-2] + T[1:Nz-1, 2:Nz]))                           
     
@@ -439,10 +446,10 @@ def PoissonSolver(rho):
     
     
         #if (jCnt % 100 == 0):
-        #    print(maxErr)
+            #print(maxErr)
     
         if maxErr < PoissonTolerance:
-            print(jCnt)
+            #print(jCnt)
             #print("Poisson solver converged")
             break
     
